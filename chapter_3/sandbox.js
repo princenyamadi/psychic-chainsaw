e5e98a3df1edab2a91c772cbb02f6c950cfd5d01
princenyamadi <princenyamadi.pn@gmail.com>
2022-01-07 19:51:48 +0000

    working with arrays

---
+++ chapter_3/sandbox.js
@@ -0,0 +1,19 @@
+let ninjas = ["shaun", "ryu", "chun-li"];
+
+// ninjas[1] = "ken";
+// console.log(ninjas[1]);
+
+// let ages = [20, 25, 30, 35];
+// console.log(ages[2]);
+
+// let random = ["shaun", "crystal", 30, 20];
+// console.log(random);
+
+// let result = ninjas.join(",");
+// console.log(result);
+
+// let result = ninjas.join('-');
+// let result = ninjas.concat(["ken", "crystal"]);
+// let result = ninjas.push("ken");
+
+// console.log(result);
